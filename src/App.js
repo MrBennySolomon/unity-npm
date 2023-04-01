de8c4d2b1a75efb1102dd091c96ebb7a2707c106
בני סולומון <mr.benny.solomon@gmail.com>
2023-04-01 13:42:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 
 function App() {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "/Build/Test.loader.js",
-    dataUrl: "/Build/Test.data.unityweb",
-    frameworkUrl: "/Build/Test.framework.js.unityweb",
-    codeUrl: "/Build/Test.wasm.unityweb",
+    loaderUrl: "/Build/Test2.loader.js",
+    dataUrl: "/Build/Test2.data.unityweb",
+    frameworkUrl: "/Build/Test2.framework.js.unityweb",
+    codeUrl: "/Build/Test2.wasm.unityweb",
   });
 
   return <Unity unityProvider={unityProvider} />;
