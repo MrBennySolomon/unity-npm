b86f60eacf7aa80e41ab82e759d2283887ece3fe
MrBennySolomon <MrBennySolomon@gmail.com>
2023-04-01 17:18:38 +0300

    unity-01-04-2023

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 
 function App() {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "/Build/Test2.loader.js",
-    dataUrl: "/Build/Test2.data.unityweb",
-    frameworkUrl: "/Build/Test2.framework.js.unityweb",
-    codeUrl: "/Build/Test2.wasm.unityweb",
+    loaderUrl: "/Build/final-1.loader.js",
+    dataUrl: "/Build/final-1.data.unityweb",
+    frameworkUrl: "/Build/final-1.framework.js.unityweb",
+    codeUrl: "/Build/final-1.wasm.unityweb",
   });
 
   return <Unity unityProvider={unityProvider} />;
