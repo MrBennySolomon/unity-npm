4a21f26b7c35fc55b2a0858417595f2d01ab8c84
בני סולומון <mr.benny.solomon@gmail.com>
2023-04-01 13:36:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "/Build/Test.loader.js",
+    dataUrl: "/Build/Test.data.unityweb",
+    frameworkUrl: "/Build/Test.framework.js.unityweb",
+    codeUrl: "/Build/Test.wasm.unityweb",
+  });
+
+  return <Unity unityProvider={unityProvider} />;
 }
 
 export default App;
